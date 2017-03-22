785bf8e0b038a33c6126f8794ec999465069e75e
joehdodd <joehdodd@gmail.com>
2017-03-22 16:58:53 -0400

    Update
    Refactored to DRY things up, namely app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 // DEFINE VIDEO AND CAPTIONS
-const video = document.getElementById('video');
+const vid = document.getElementById('video');
 const captions = document.querySelectorAll('p span');
 
-// SET CAPTIONS CLASSNAME TO EMPTY STRING, JUST IN CASE!
-captions.className = '';
+// ADD EVENT LISTENER TO VID
+vid.addEventListener('timeupdate', () => {
+  // LOOP OVER CAPTIONS (QUERYSELECTORALL COLLECTION)
+  for(let i = 0; i < captions.length; ++i) {
+    // DEFINE TIME IN LOCAL SCOPE TO PROPERLY GET VALUE
+    let time = vid.currentTime;
+    // DEFINE START USING ARRAY INDEX
+    let start = captions[i].getAttribute('data-start');
+    // DEFINE END USING ARRAY INDEX
+    let end = captions[i].getAttribute('data-end');
+      // SET CONDITIONAL TO CHECK START AND END AGAINST TIME
+      if (time >= start && time <= end) {
+        // IF && IS TRUE, ADD HIGHLIGHT
+        captions[i].className = 'highlight';
+      } else {
+        // OTHERWISE SET CLASS TO EMPTY STRING
+        captions[i].className = '';
+      }
+  }
+});
 
 // ADD EVENT LISTENER TO VIDEO
-video.addEventListener('timeupdate', () => {
+/*video.addEventListener('timeupdate', () => {
     // Add Class Caption 1
     if (video.currentTime >= 0.240 && video.currentTime <= 4.130) {
       captions[0].className = 'highlight';
@@ -73,4 +91,4 @@ video.addEventListener('timeupdate', () => {
     } else {
       captions.className = '';
     }
-});
+});*/
